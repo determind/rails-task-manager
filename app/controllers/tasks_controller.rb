34dc518f651cfc8f2ded6f8e0943e7bad0d67f5d
patrick lambret <patrick@happy-dev.fr>
2018-11-20 16:23:48 +0100

    working but no style and no refactoring, will get back to it one day

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,21 +4,44 @@ class TasksController < ApplicationController
   end
 
   def show          # GET /tasks/:id
-    @tasks = Tasks.find(params[:id])
+    @tasks = Task.find(params[:id])
   end
 
   def new           # GET /tasks/new
+    @task = Task.new
   end
 
   def create        # POST /tasks
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
   end
 
-  def edit          # GET /tasks/:id/edit
+  def edit        # GET /tasks/:id/edit
+    @task = Task.find(params[:id])
   end
 
   def update        # PATCH /tasks/:id
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
   end
 
   def destroy       # DELETE /tasks/:id
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
